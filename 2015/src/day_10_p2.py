04346e20bd5b374bbe24b8576557a81c0b68b4f5
Zanfer322 <34981417+Zanfer322@users.noreply.github.com>
2025-06-01 17:26:29 +0530

    add 2015 day 10

---
+++ 2015/src/day_10_p2.py
@@ -0,0 +1,20 @@
+import itertools
+
+from libs import io
+
+
+def _look_and_say(xs: str) -> str:
+    say_arr = []
+    for key, group in itertools.groupby(xs):
+        group_len = len(list(group))
+        say_arr.append(str(group_len))
+        say_arr.append(key)
+    return "".join(say_arr)
+
+
+input = io.read_all()
+
+ans = input
+for _ in range(50):
+    ans = _look_and_say(ans)
+print(len(ans))
