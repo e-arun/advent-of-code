23fadc16ba15a45eaa8ea721f8fa90a2be020c0e
Zanfer322 <34981417+Zanfer322@users.noreply.github.com>
2025-06-04 22:06:58 +0530

    add 2016 day 6

---
+++ 2016/src/day_06_p1.ts
@@ -0,0 +1,20 @@
+const mostCommon = (arr: string[]): string => {
+	const map = new Map<string, number>();
+	for (const item of arr) {
+		map.set(item, (map.get(item) ?? 0) + 1);
+	}
+	return map
+		.entries()
+		.toArray()
+		.sort((a, b) => (a[1] < b[1] ? 1 : -1))[0][0];
+};
+
+const input = await Bun.stdin.text();
+const lines = input.trim().split("\n");
+
+const ans: string[] = [];
+for (let i = 0; i < lines[0].length; i++) {
+	const char = mostCommon(lines.map((x) => x[i]));
+	ans.push(char);
+}
+console.log(ans.join(""));
