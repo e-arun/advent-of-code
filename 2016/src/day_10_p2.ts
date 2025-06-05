8f3ae7875382784ccc1897ff5b0d20d1e4cdde67
Zanfer322 <34981417+Zanfer322@users.noreply.github.com>
2025-06-05 09:09:00 +0530

    add 2016 day 10

---
+++ 2016/src/day_10_p2.ts
@@ -0,0 +1,72 @@
+type Instr =
+	| { kind: "value"; value: number; bot: number }
+	| { kind: "bot"; bot: number; low: number; high: number };
+
+const parseLine = (line: string): Instr => {
+	const words = line.split(" ");
+	if (words[0] === "value") {
+		return { kind: "value", value: Number(words[1]), bot: Number(words[5]) };
+	}
+
+	let low = Number(words[6]);
+	if (words[5] === "output") {
+		low = -low - 1; // extra -1 to disambiguate between bot 0 and output 0
+	}
+
+	let high = Number(words[11]);
+	if (words[10] === "output") {
+		high = -high - 1; // extra -1 to disambiguate between bot 0 and output 0
+	}
+
+	return {
+		kind: "bot",
+		bot: Number(words[1]),
+		low,
+		high,
+	};
+};
+
+const input = await Bun.stdin.text();
+const instrArr = input.trim().split("\n").map(parseLine);
+const botChips = new Map<number, number[]>();
+
+const addChips = (bot: number, chip: number) => {
+	const curChips = botChips.get(bot) ?? [];
+	if (curChips.length > 1) {
+		throw "Too many chips to hold";
+	}
+	curChips.push(chip);
+	botChips.set(bot, curChips);
+};
+
+for (const instr of instrArr) {
+	if (instr.kind === "value") {
+		addChips(instr.bot, instr.value);
+	}
+}
+
+mainLoop: while (true) {
+	for (const instr of instrArr) {
+		if (instr.kind === "value") {
+			continue;
+		}
+		const curChips = botChips.get(instr.bot) ?? [];
+		if (curChips.length !== 2) {
+			continue;
+		}
+		curChips.sort((a, b) => a - b);
+		if (curChips[0] > curChips[1]) {
+			throw curChips;
+		}
+		addChips(instr.low, curChips[0]);
+		addChips(instr.high, curChips[1]);
+		botChips.set(instr.bot, []);
+
+		if (botChips.has(-1) && botChips.has(-2) && botChips.has(-3)) {
+			const ans =
+				botChips.get(-1)![0] * botChips.get(-2)![0] * botChips.get(-3)![0];
+			console.log(ans);
+			break mainLoop;
+		}
+	}
+}
