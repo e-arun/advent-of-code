b962bf7e762b3eb6b4e221c234c68352aae51d6e
Zanfer322 <34981417+Zanfer322@users.noreply.github.com>
2025-06-07 10:38:51 +0530

    add 2016 day 16

---
+++ 2016/src/day_16_p2.ts
@@ -0,0 +1,31 @@
+import { batched } from "./lib";
+
+const dragon = (a: string): string => {
+	const b = a
+		.split("")
+		.reverse()
+		.map((x) => (x === "0" ? "1" : "0"));
+	return `${a}0${b.join("")}`;
+};
+
+const checksum = (a: string): string => {
+	return batched(a, 2)
+		.map(([x, y]) => (x === y ? "1" : "0"))
+		.join("");
+};
+
+let input = await Bun.stdin.text();
+input = input.trim();
+const n = 35651584;
+
+let ans = input;
+while (ans.length < n) {
+	ans = dragon(ans);
+}
+ans = ans.slice(0, n);
+
+ans = checksum(ans);
+while (ans.length % 2 === 0) {
+	ans = checksum(ans);
+}
+console.log(ans);
