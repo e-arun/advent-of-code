060b70757f329dc71d1b459b1c5067c929847aa7
Zanfer322 <34981417+Zanfer322@users.noreply.github.com>
2025-06-07 12:43:49 +0530

    add 2016 day 18

---
+++ 2016/src/day_18_p1.ts
@@ -0,0 +1,36 @@
+const getNextRow = (row: string): string => {
+	const newRow: string[] = [];
+
+	for (let i = 0; i < row.length; i++) {
+		const a = row[i - 1] ?? ".";
+		const b = row[i];
+		const c = row[i + 1] ?? ".";
+		const section = [a, b, c].join("");
+
+		if (["^^.", ".^^", "^..", "..^"].includes(section)) {
+			newRow.push("^");
+		} else {
+			newRow.push(".");
+		}
+	}
+
+	return newRow.join("");
+};
+
+const input = await Bun.stdin.text().then((x) => x.trim());
+const rows = 40;
+
+const mat = [input];
+for (let i = 1; i < rows; i++) {
+	mat.push(getNextRow(mat[i - 1]));
+}
+
+let ans = 0;
+for (const row of mat) {
+	for (const chr of row) {
+		if (chr === ".") {
+			ans += 1;
+		}
+	}
+}
+console.log(ans);
