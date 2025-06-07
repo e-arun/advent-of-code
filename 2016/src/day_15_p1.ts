6369a8d88a829b234add2401ace33ff672c48bc4
Zanfer322 <34981417+Zanfer322@users.noreply.github.com>
2025-06-07 10:28:11 +0530

    add 2016 day 15

---
+++ 2016/src/day_15_p1.ts
@@ -0,0 +1,25 @@
+type Disc = { positions: number; startPos: number };
+
+const parseLine = (line: string): Disc => {
+	const words = line.split(" ");
+	const positions = Number(words[3]);
+	const startPos = Number(words[words.length - 1].replace(".", ""));
+	return { positions, startPos };
+};
+
+const input = await Bun.stdin.text();
+const discs = input.trim().split("\n").map(parseLine);
+
+let ans = -1;
+mainLoop: while (true) {
+	ans += 1;
+
+	for (const [i, disc] of discs.entries()) {
+		if ((ans + i + 1 + disc.startPos) % disc.positions !== 0) {
+			continue mainLoop;
+		}
+	}
+	break;
+}
+
+console.log(ans);
