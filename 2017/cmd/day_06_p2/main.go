e033156dcae7bcd9623dad555f671f9eda7e0626
Zanfer322 <34981417+Zanfer322@users.noreply.github.com>
2025-06-09 21:28:14 +0530

    add 2017 day 6

---
+++ 2017/cmd/day_06_p2/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func hashBlocks(blocks []int) string {
+	strArr := make([]string, len(blocks))
+	for i, val := range blocks {
+		strArr[i] = strconv.Itoa(val)
+	}
+	return strings.Join(strArr, "|")
+}
+
+func redistribute(blocks []int) {
+	maxVal, maxIdx := blocks[0], 0
+	for i, val := range blocks {
+		if val > maxVal {
+			maxVal = val
+			maxIdx = i
+		}
+	}
+	blocks[maxIdx] = 0
+	for i := range maxVal {
+		blocks[(maxIdx+1+i)%len(blocks)] += 1
+	}
+}
+
+func main() {
+	input, _ := io.ReadAll(os.Stdin)
+	input = bytes.TrimSpace(input)
+
+	blocks := make([]int, 0)
+	for line := range strings.SplitSeq(string(input), "\t") {
+		num, _ := strconv.Atoi(line)
+		blocks = append(blocks, num)
+	}
+
+	vis := make(map[string]bool)
+	vis[hashBlocks(blocks)] = true
+	var tarHash string
+
+	for {
+		redistribute(blocks)
+		hash := hashBlocks(blocks)
+
+		if vis[hash] {
+			tarHash = hash
+			break
+		}
+		vis[hash] = true
+	}
+
+	ans := 0
+	for {
+		ans += 1
+		redistribute(blocks)
+		if hashBlocks(blocks) == tarHash {
+			break
+		}
+	}
+	fmt.Println(ans)
+}
