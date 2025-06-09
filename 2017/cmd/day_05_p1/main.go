2ba6d7011de9a040aba037d7c059b2324fb8cc20
Zanfer322 <34981417+Zanfer322@users.noreply.github.com>
2025-06-09 21:06:39 +0530

    add 2017 day 5

---
+++ 2017/cmd/day_05_p1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, _ := io.ReadAll(os.Stdin)
+	input = bytes.TrimSpace(input)
+
+	jumps := make([]int, 0)
+	for line := range strings.SplitSeq(string(input), "\n") {
+		num, _ := strconv.Atoi(line)
+		jumps = append(jumps, num)
+	}
+
+	ans := 0
+	i := 0
+	for i >= 0 && i < len(jumps) {
+		ans += 1
+		jumps[i] += 1
+		i += jumps[i] - 1
+
+	}
+	fmt.Println(ans)
+}
