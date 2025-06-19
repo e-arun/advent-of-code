129403ab6f9d91428984d33c8749485540923ae1
Zanfer322 <34981417+Zanfer322@users.noreply.github.com>
2025-06-19 22:22:27 +0530

    add 2018 day 3

---
+++ 2018/src/bin/day_03_p1.rs
@@ -0,0 +1,52 @@
+use std::str::FromStr;
+
+use regex::Regex;
+
+#[derive(Debug)]
+struct Claim {
+    left: usize,
+    top: usize,
+    width: usize,
+    height: usize,
+}
+
+impl FromStr for Claim {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+        let caps = re.captures(s).unwrap();
+
+        let claim = Claim {
+            left: caps[2].parse().unwrap(),
+            top: caps[3].parse().unwrap(),
+            width: caps[4].parse().unwrap(),
+            height: caps[5].parse().unwrap(),
+        };
+        Ok(claim)
+    }
+}
+
+fn main() {
+    let lines = aoc::read_lines();
+    let claims: Vec<Claim> = lines.into_iter().map(|x| x.parse().unwrap()).collect();
+
+    let mut mat = [[0; 1200]; 1200];
+    for claim in claims {
+        for x in claim.left..(claim.left + claim.width) {
+            for y in claim.top..(claim.top + claim.height) {
+                mat[x][y] += 1;
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..1200 {
+        for j in 0..1200 {
+            if mat[i][j] >= 2 {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
+}
